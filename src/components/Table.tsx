7cdbc5cda5dd0027da85b0a3369292a7f329f662
jaumdev1 <joao280799@gmail.com>
2024-04-23 18:25:04 -0300

    fix: responsive layout home

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -16,7 +16,7 @@ const Table: React.FC<TableProps> = ({ posts }) => {
   return (
     <div className="text-white font-roboto  w-full flex flex-col items-center h-full p-20">
 
-  <div className='flex flex-col gap-5'>
+  <div className='flex flex-col gap-5 w-screen justify-center items-center sm:justify-start sm:items-start sm:w-auto p-4'>
   <h1 className='text-white font-roboto'>Posts</h1>
   {posts.map((post, index) => (
     <Post key={index} id={post.id} author={post.author} createdAt={post.createdAt} title={post.title} />
