16fac8babc715539ec29cc3b43b0e0d7bbd9b1df
jaumdev1 <joao280799@gmail.com>
2024-04-23 20:50:06 -0300

    feat: change uri api

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <div className="text-white font-roboto  w-full flex items-end justify-center h-full p-2">
-<p className='text-center'>"Count on the kindness of human beings. But wait until you can see the results of that kindness."</p>
+    <p className='text-center'>"Count on the kindness of human beings. But wait until you can see the results of that kindness."</p>
  
 </div>
   );
